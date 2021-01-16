2013a92cef90ea0232472f608ad00f59f9eed236
Chelsea Becker <cschweig2@gmail.com>
2021-01-15 16:33:20 -0800

    add delete joinid routes

--- Pierre/Controllers/FlavorsController.cs
+++ Pierre/Controllers/FlavorsController.cs
@@ -134,5 +134,14 @@ namespace Pierre.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        public ActionResult DeleteTreat(int joinId)
+        {
+            var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+            _db.FlavorTreat.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
