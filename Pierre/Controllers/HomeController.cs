070153428510a7bf3890bc854d879462bda47df3
Chelsea Becker <cschweig2@gmail.com>
2021-01-15 21:15:00 -0800

    add dictionary to display treats and flavors

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -9,10 +9,22 @@ namespace Pierre.Controllers
 {
     public class HomeController: Controller
     {
+        private readonly PierreContext _db;
+
+        public HomeController(PierreContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet("/")]
         public ActionResult Index()
         {
-            return View();
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            List<Flavor> flavors = _db.Flavors.ToList();
+            List<Treat> treats = _db.Treats.ToList();
+            model.Add("flavors", flavors);
+            model.Add("treats", treats);
+            return View(model);
         }
     }
 }
